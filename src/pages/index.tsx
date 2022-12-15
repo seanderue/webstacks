045b84755f357efc3681b7410114c94716e715fc
Sean <71807040+seanderue@users.noreply.github.com>
2022-12-15 17:44:15 -0500

    index is now /blob & readded plane reflector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,109 @@
-import dynamic from 'next/dynamic'
-// import { ReactDOM } from 'react-dom/client'
-import Instructions from '@/components/dom/Instructions'
-import IndependentCanvas from '@/components/canvas/IndependentCanvas'
-
 // Dynamic import is used to prevent a payload when the website starts, that includes threejs, r3f etc..
 // WARNING ! errors might get obfuscated by using dynamic import.
 // If something goes wrong go back to a static import to show the error.
 // https://github.com/pmndrs/react-three-next/issues/49
-const Logo = dynamic(() => import('@/components/canvas/Logo'), { ssr: false })
+import dynamic from 'next/dynamic'
+import { Perf } from 'r3f-perf'
+import { useState } from 'react'
+import { Environment, PresentationControls } from '@react-three/drei'
+import Effects from '@/components/canvas/Effects'
+import ExperienceBase from '@/components/canvas/ExperienceBase'
+import Image from 'next/image'
+// import CryptoCoin from '@/components/canvas/CryptoCoin'
 
-// const root = ReactDOM.createRoot(document.querySelector('#root-experience'))
+// const Blob = dynamic(() => import('@/components/canvas/Blob'), { ssr: false })
 
-// Dom components go here
 export default function Page(props) {
+  const [isInteracting, setIsInteracting] = useState(false)
+  const toggleIsInteracting = () => {
+    setIsInteracting((prev) => !prev)
+  }
+
   return (
     <>
-      <Instructions>
-        This is a minimal starter for Nextjs + React-three-fiber and Threejs. Click on the{' '}
-        <span className='text-cyan-200'>atoms nucleus</span> to navigate to the{' '}
-        <span className='text-green-200'>/blob</span> page. OrbitControls are enabled by default.
-      </Instructions>
-      <div id='root-experience'></div>
-      <IndependentCanvas ref={props.ref} route={'/'} />
+      <section className={`landing ${isInteracting ? 'is-interacting' : 'is-not-interacting'}`}>
+        {/* <Overlay /> */}
+        <div className='mask is-top' />
+        <div onClick={toggleIsInteracting} className='mask is-right' />
+        <div className='mask is-bottom' />
+        <div className='mask is-left' />
+        <div className='content delay-1'>
+          <h1 className='element main-title delay-0'>
+            Your Next React Engineer
+            {/* <br /> Hire a React Engineer */}
+          </h1>
+          <a href='https://www.linkedin.com/in/sean-derue/' className='element author delay-1'>
+            <Image className='picture' src='/img/author.jpeg' alt={'Sean DeRue'} width={64} height={64} />
+            <span className='label'>
+              A Cover-Letter Experience <br /> by <span className='underline-container'>Sean DeRue</span>
+            </span>
+          </a>
+          <p className='element description delay-2'>
+            I want to be a part of Webstacks&apos; next success stories. I made this site to prove it.
+          </p>
+          <p className='element description delay-3'>
+            ( Built in 20 days using Typescript, React, Next.js, Three.js, with responsive, accessible, and SEO
+            friendliness in mind )
+          </p>
+          <p className='element description delay-4'>Lorem Ipsum</p>
+        </div>
+        <div className={'experience'} onMouseDown={!isInteracting ? toggleIsInteracting : () => {}}>
+          {props.children}
+          <button className='click' aria-label='Explore'>
+            <span className='hover' />
+            <span className='label'>Click to explore</span>
+          </button>
+        </div>
+        <button className='close' aria-label='Close' onClick={toggleIsInteracting}>
+          <span className='background' />
+          <span className='bars'>
+            <span className='bar bar-1' />
+            <span className='bar bar-2' />
+          </span>
+        </button>
+      </section>
+      <section className='presentation'>
+        <div className='content'>
+          <h1 style={{ textAlign: 'center' }}>Temporary styling within temp div on presentation</h1>
+        </div>
+      </section>
     </>
   )
 }
 
-// Canvas components go here
-// It will receive same props as the Page component (from getStaticProps, etc.)
-Page.canvas = (props) => <Logo scale={0.5} route='/blob' position-y={-1} />
+Page.canvas = (props) => (
+  <>
+    <group position={[0, -3, 0]}>
+      <Perf position='top-left' />
 
-export async function getStaticProps() {
-  return { props: { title: 'Index' } }
-}
+      {/* Staging */}
+      {/* <directionalLight shadow-bias={0.1} castShadow intensity={2} position={[10, 6, 6]} shadow-mapSize={[1024, 1024]}>
+        <orthographicCamera attach='shadow-camera' left={-20} right={20} top={20} bottom={-20} />
+      </directionalLight> */}
+
+      <Effects />
+      <Environment preset='city' />
+
+      {/* Meshes */}
+      {/* <Blob route='/' position-y={3.75} /> */}
+      <PresentationControls
+        global
+        cursor={true}
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.2, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}>
+        <ExperienceBase />
+      </PresentationControls>
+
+      {/* 3D Text */}
+      {/* <Text rotation={[0, 0, 0]} position={[-2.5, 0, 0]}>
+        [ ]
+      </Text> */}
+    </group>
+  </>
+)
+
+// export async function getStaticProps() {
+//   return { props: { title: 'Blob' } }
+// }
