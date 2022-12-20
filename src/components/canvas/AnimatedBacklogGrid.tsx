c8de6eda52176fd88ebc37e43b437d718a35378c
Sean <71807040+seanderue@users.noreply.github.com>
2022-12-19 22:13:23 -0500

    created navigation and chapters

--- src/components/canvas/AnimatedBacklogGrid.tsx
+++ src/components/canvas/AnimatedBacklogGrid.tsx
@@ -1,32 +1,35 @@
-type Position = SpringValue<[x: number, y: number, z: number]>
-
-function AnimatedBox({ position }: { position: [number, number, number] }) {
-  const spring: Position = useSpring({
-    to: async (next, cancel) => {
-      await next({ y: position[1] + 2 })
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      await next({ y: position[1] + 4 })
-      await new Promise((resolve) => setTimeout(resolve, 1000))
-      cancel()
-    },
-    from: { y: position[1] },
-  })
-
-  return (
-    <animated.mesh position={[0, spring, 0]}>
-      <boxGeometry attach='geometry' args={[1, 1, 0.25]} />
-      <meshBasicMaterial attach='material' color='#00ff00' />
-    </animated.mesh>
-  )
+export default function Component() {
+  return
 }
+// type Position = SpringValue<[x: number, y: number, z: number]>
 
-function Grid() {
-  const boxes = []
+// function AnimatedBox({ position }: { position: [number, number, number] }) {
+//   const spring: Position = useSpring({
+//     to: async (next, cancel) => {
+//       await next({ y: position[1] + 2 })
+//       await new Promise((resolve) => setTimeout(resolve, 1000))
+//       await next({ y: position[1] + 4 })
+//       await new Promise((resolve) => setTimeout(resolve, 1000))
+//       cancel()
+//     },
+//     from: { y: position[1] },
+//   })
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      boxes.push(<AnimatedBox position={[i, 0, j]} key={`box-${i}-${j}`} />)
-    }
-  }
+//   return (
+//     <animated.mesh position={[0, spring, 0]}>
+//       <boxGeometry attach='geometry' args={[1, 1, 0.25]} />
+//       <meshBasicMaterial attach='material' color='#00ff00' />
+//     </animated.mesh>
+//   )
+// }
 
-  return <group>{boxes}</group>
+// function Grid() {
+//   const boxes = []
+
+//   for (let i = 0; i < 10; i++) {
+//     for (let j = 0; j < 10; j++) {
+//       boxes.push(<AnimatedBox position={[i, 0, j]} key={`box-${i}-${j}`} />)
+//     }
+//   }
+
+//   return <group>{boxes}</group>
